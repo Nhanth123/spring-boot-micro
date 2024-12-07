1103e1a6bb5cac70d0c576009604362e38ec3c19
nhan.trinh <hongnhan50@gmail.com>
2024-12-07 21:07:24 +0700

    Setup project

---
+++ micro-brewery/src/main/java/api/spring/micro_brewery/MicroBreweryApplication.java
@@ -0,0 +1,13 @@
+package api.spring.micro_brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBreweryApplication.class, args);
+	}
+
+}
---
+++ micro-brewery/src/test/java/api/spring/micro_brewery/MicroBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package api.spring.micro_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
